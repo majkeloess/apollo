ca69753ae2fc03549ff9242544ffd1e24dc453bd
majkeloess <msaturczak@student.agh.edu.pl>
2024-05-10 15:54:11 +0200

    dashboard creating

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,5 +1,82 @@
 const { db } = require('@vercel/postgres');
-const { exercises } = require("../lib/data.js");
+const { exercises, articles, playlists } = require("../lib/data.js");
+
+async function seedArticles(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS articles(
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        link TEXT NOT NULL,
+        note TEXT NOT NULL
+      )
+    `;
+
+    console.log("Created articles table!");
+
+    const insertArticles = await Promise.all(
+      articles.map(
+        (article) => client.sql`
+          INSERT INTO articles (name, link, note)
+          VALUES(${article.name}, ${article.link}, ${article.note})
+          `
+      )
+    );
+
+    console.log("Inserted into articles!");
+
+  } catch (error) {
+    console.error('Error seeding articles:', error);
+    throw error;
+  }
+}
+
+
+
+async function seedPlaylist(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+    const createTable = client.sql`
+    CREATE TABLE IF NOT EXISTS playlists(
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name TEXT NOT NULL, 
+      link TEXT NOT NULL, 
+      genre VARCHAR(255)
+    )
+    `;
+
+    console.log("Created playlist table!");
+
+    const insertPlaylist = await Promise.all(
+      playlists.map(
+        (playlist) => client.sql`
+        INSERT INTO playlists (name, link, genre)
+        VALUES (${playlist.name}, ${playlist.link}, ${playlist.genre})
+        `
+      )
+    );
+
+    console.log("Inserted into playlist!")
+
+  } catch (error) {
+    console.error('Error seeding playlists:', error);
+    throw error;
+  }
+}
+
+async function insertExer(client) {
+  const insertExercises = await Promise.all(
+    exercises.map(
+      (exercise) => client.sql`
+        INSERT INTO exercises (name, muscleGroup)
+        VALUES(${exercise.name}, ${exercise.group})
+        `
+    ),
+  );
+}
 
 
 async function seedExercises(client) {
@@ -37,9 +114,10 @@ async function seedExercises(client) {
 async function main() {
   const client = await db.connect();
 
-
+  await seedPlaylist(client);
+  await seedArticles(client);
   await seedExercises(client);
-  console.log("Test");
+  await insertExer(client);
 
   await client.end();
 }

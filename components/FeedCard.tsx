0a00d4eaddd8a073883e53609dd84e78d95428bf
majkeloess <msaturczak@student.agh.edu.pl>
2024-05-11 21:10:53 +0200

    Feed Card

--- components/FeedCard.tsx
+++ components/FeedCard.tsx
@@ -32,7 +32,7 @@ export default function FeedCard({
   photo: string;
 }) {
   return (
-    <div className="w-[300px] xl:w-[600px]">
+    <div className="w-[350px] lg:w-[600px]">
       <Card>
         <CardHeader className="flex flex-row gap-3 items-center">
           <Avatar>
@@ -48,25 +48,41 @@ export default function FeedCard({
           </div>
         </CardHeader>
         <CardContent>
-          <p className="text-lg">{description}</p>
-          <div className="flex flex-row w-full justify-between py-4">
+          <p className="text-lg font-medium">{description}</p>
+          <div className="h-0.5 my-4 dark:bg-gray-800 bg-gray-200"></div>
+          <div className="flex flex-row w-full lg:justify-evenly justify-between">
             <ul>
-              <li className="text-gray-400 text-sm">Total load</li>
+              <li
+                className="dark:text-gray-400 text-gray-500
+               text-sm"
+              >
+                Total load
+              </li>
               <li className="font-bold">{load} kg</li>
             </ul>
             <ul>
-              <li className="text-gray-400 text-sm">Sets/Reps</li>
+              <li
+                className="dark:text-gray-400 text-gray-500
+               text-sm"
+              >
+                Sets/Reps
+              </li>
               <li className="font-bold">
                 {sets}/{reps}
               </li>
             </ul>
             <ul>
-              <li className="text-gray-400 text-sm">Time</li>
+              <li
+                className="dark:text-gray-400 text-gray-500
+               text-sm"
+              >
+                Time
+              </li>
               <li className="font-bold">{time}</li>
             </ul>
           </div>
           <p className="text-lg"></p>
-          <div className="w-full flex justify-center">
+          <div className="w-full flex justify-center pt-6">
             <Image
               src={photo}
               width={280}
@@ -77,7 +93,7 @@ export default function FeedCard({
           </div>
         </CardContent>
         <CardFooter>
-          <ul className="flex flex-row w-full justify-evenly">
+          <ul className="flex flex-row w-full lg:justify-center lg:gap-10 justify-evenly">
             <li>
               <Button variant="outline">
                 <IconBolt />

b3d91e127f19686686c86133751bf1d0ced228da
majkeloess <msaturczak@student.agh.edu.pl>
2024-05-13 22:13:53 +0200

    Prisma update

--- auth.ts
+++ auth.ts
@@ -1,9 +1,17 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
+
+export const runtime = "edge";
+
+const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
+const adapter = new PrismaNeon(neon);
+const prisma = new PrismaClient({ adapter });
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),

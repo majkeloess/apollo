f2276325c40a8f8d15f68626a21577d9859e2075
majkeloess <msaturczak@student.agh.edu.pl>
2024-05-24 16:47:56 +0200

    New Exer Db

--- app/(detailed)/articles/[articleId]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function ArticlePage({
-  params,
-}: {
-  params: { articleId: string };
-}) {
-  return <div>article</div>;
-}
--- app/(detailed)/music/[musicId]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function MusicPage({
-  params,
-}: {
-  params: { musicId: string };
-}) {
-  return <div>music</div>;
-}
